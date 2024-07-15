a537a7d4b17c1f13d4415265af0cc8edd59d923e
Tanishq2903 <maharshitanishq@gmail.com>
2024-07-15 20:55:39 +0530

    typos fixed

--- client/src/components/pages/Authentication/Login.jsx
+++ client/src/components/pages/Authentication/Login.jsx
@@ -26,7 +26,7 @@ function Login({ setUserWantsToLogin }) {
   return (
     <div className="basis-[35rem]">
       <h1 className="text-cta-icon font-semibold text-[2rem] uppercase mb-[2rem]">
-        Login To Telegram
+        Login To Tonygram
       </h1>
       <Formik
         initialValues={{
@@ -78,7 +78,7 @@ function Login({ setUserWantsToLogin }) {
         onClick={() => setUserWantsToLogin(false)}
         className="mt-[2rem] text-right  text-secondary-text underline cursor-pointer hover:text-cta-icon"
       >
-        New to Telegram
+        New to Tonygram
       </div>
     </div>
   );
